f0ff7c1c4104ce020453731725b9022a202dd0d3
Mars <mars@substantial.com>
2014-09-07 21:38:25 -0700

    Install FayeClient

---
+++ Podfile
@@ -0,0 +1,10 @@
+platform :ios, "7.0"
+
+target "Dexr" do
+    pod "MZFayeClient"
+    pod "SocketRocket"
+    #pod "JSONKit", git: 'https://github.com/postmates/JSONKit', commit: 'e42fbf66b58d9d75c87f8b1d5ebb2b4667be1332'
+end
+
+target "DexrTests" do
+end
---
+++ Pods/Pods-environment.h
@@ -0,0 +1,21 @@
+
+// To check if a library is compiled with CocoaPods you
+// can use the `COCOAPODS` macro definition which is
+// defined in the xcconfigs so it is available in
+// headers also when they are imported in the client
+// project.
+
+
+// JSONKit
+#define COCOAPODS_POD_AVAILABLE_JSONKit
+// This library does not follow semantic-versioning,
+// so we were not able to define version macros.
+// Please contact the author.
+// Version: 1.5pre.
+
+// SocketRocket
+#define COCOAPODS_POD_AVAILABLE_SocketRocket
+#define COCOAPODS_VERSION_MAJOR_SocketRocket 0
+#define COCOAPODS_VERSION_MINOR_SocketRocket 3
+#define COCOAPODS_VERSION_PATCH_SocketRocket 1
+
