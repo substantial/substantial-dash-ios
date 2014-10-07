ef313f0cfa12489e4aa77877e92ba341bdf6467b
Mars <mars@substantial.com>
2014-10-06 23:13:50 -0700

    Install ReactiveCocoa

--- Podfile
+++ Podfile
@@ -1,6 +1,7 @@
 platform :ios, "7.0"
 
 target "Dexr" do
+    pod "ReactiveCocoa"
     pod "MZFayeClient"
     pod "SocketRocket"
     #pod "JSONKit", git: 'https://github.com/postmates/JSONKit', commit: 'e42fbf66b58d9d75c87f8b1d5ebb2b4667be1332'
--- Pods/Pods-Dexr-environment.h
+++ Pods/Pods-Dexr-environment.h
@@ -24,6 +24,30 @@
 #define COCOAPODS_VERSION_MINOR_NSURL_QueryDictionary 0
 #define COCOAPODS_VERSION_PATCH_NSURL_QueryDictionary 3
 
+// ReactiveCocoa
+#define COCOAPODS_POD_AVAILABLE_ReactiveCocoa
+#define COCOAPODS_VERSION_MAJOR_ReactiveCocoa 2
+#define COCOAPODS_VERSION_MINOR_ReactiveCocoa 3
+#define COCOAPODS_VERSION_PATCH_ReactiveCocoa 1
+
+// ReactiveCocoa/Core
+#define COCOAPODS_POD_AVAILABLE_ReactiveCocoa_Core
+#define COCOAPODS_VERSION_MAJOR_ReactiveCocoa_Core 2
+#define COCOAPODS_VERSION_MINOR_ReactiveCocoa_Core 3
+#define COCOAPODS_VERSION_PATCH_ReactiveCocoa_Core 1
+
+// ReactiveCocoa/UI
+#define COCOAPODS_POD_AVAILABLE_ReactiveCocoa_UI
+#define COCOAPODS_VERSION_MAJOR_ReactiveCocoa_UI 2
+#define COCOAPODS_VERSION_MINOR_ReactiveCocoa_UI 3
+#define COCOAPODS_VERSION_PATCH_ReactiveCocoa_UI 1
+
+// ReactiveCocoa/no-arc
+#define COCOAPODS_POD_AVAILABLE_ReactiveCocoa_no_arc
+#define COCOAPODS_VERSION_MAJOR_ReactiveCocoa_no_arc 2
+#define COCOAPODS_VERSION_MINOR_ReactiveCocoa_no_arc 3
+#define COCOAPODS_VERSION_PATCH_ReactiveCocoa_no_arc 1
+
 // SocketRocket
 #define COCOAPODS_POD_AVAILABLE_SocketRocket
 #define COCOAPODS_VERSION_MAJOR_SocketRocket 0
